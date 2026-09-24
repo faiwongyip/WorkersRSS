cb57500d47dfbe7f9a20f177e6c1607c1b969c8c
faiwongyip <981104072@qq.com>
2026-09-24 12:53:13 +0800

    Add files via upload

---
+++ src/routers/_registry.js
@@ -0,0 +1,23 @@
+// 本文件由 scripts/gen-routers.mjs 自动生成，请勿手动修改。
+
+import {cospuri} from "./cospuri.js"
+import {dlsite} from "./dlsite.js"
+import {fellatiojapan} from "./fellatiojapan.js"
+import {github} from "./github.js"
+import {handjobjapan} from "./handjobjapan.js"
+import {iwara} from "./iwara.js"
+import {nhentai} from "./nhentai.js"
+import {pawchive} from "./pawchive.js"
+import {wikipediahot} from "./wikipediahot.js"
+
+export const routers = {
+    cospuri,
+    dlsite,
+    fellatiojapan,
+    github,
+    handjobjapan,
+    iwara,
+    nhentai,
+    pawchive,
+    wikipediahot,
+};
